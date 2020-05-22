4d3f340553409f9c2f72b7c993be1b50090f21d2
w0rng <antonabramov2000@gmail.com>
2020-05-22 19:14:07 +0700

    Поправил багулю с месяцем

--- reps.js
+++ reps.js
@@ -4,11 +4,11 @@ xhr.send();
 
 var resp = JSON.parse(xhr.responseText);
 
-function sortByAge(arr) {
+function sortByTime(arr) {
     arr.sort((a, b) => a["created_at"] > b["created_at"] ? 1 : -1);
 }
 
-sortByAge(resp);
+sortByTime(resp);
 
 resp.forEach(function (item, i, arr) {
     if (item["language"] == "Python") {
@@ -24,8 +24,8 @@ resp.forEach(function (item, i, arr) {
         projects.insertAdjacentHTML("afterbegin",
             "🔥 <a href=\"" + url + "\">" + name + "</a>" +
             "<span class=\"click\"> (клик)<br>" +
-            start.getDate() + "." + start.getMonth() + "." + start.getFullYear() + " - " +
-            stop.getDate() + "." + stop.getMonth() + "." + stop.getFullYear() +
+            start.getDate() + "." + (+start.getMonth() + 1) + "." + start.getFullYear() + " - " +
+            stop.getDate() + "." + (+stop.getMonth() + 1) + "." + stop.getFullYear() +
             "</span><h4>" + description + "</h4><br>");
     }
 });
